e0e0454f19cfc9816c68969b5adeea6d0a2b8e6c
joshpson <jpearson846@gmail.com>
2018-07-20 15:07:23 -0400

    integrated connected react router

--- app/models/google_map.rb
+++ app/models/google_map.rb
@@ -6,5 +6,11 @@ class GoogleMap < ApplicationRecord
     JSON.parse(response.body)
   end
 
+  def self.getCoords(place_id)
+    response = Faraday.get "https://maps.googleapis.com/maps/api/place/details/json?placeid=#{place_id}&key=#{Rails.application.credentials.google_maps}"
+    json = JSON.parse(response.body)
+    json["result"]["geometry"]["location"]
+  end
+
 
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,11 @@
 class Location < ApplicationRecord
   belongs_to :user
+  before_validation :fetch_geodata
+
+  def fetch_geodata
+    location = GoogleMap.getCoords(self.place_id)
+    self.latitude = location["lat"]
+    self.longitude = location["lng"]
+  end
+
 end
